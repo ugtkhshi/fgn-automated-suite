0f419ab8805f56afca9148f372d7d3213cfe7f4b
Yuji Takahashi <yuji.takahashi@arcadian.la>
2026-06-30 11:05:48 +0800

    refactored login.spec.ts - adding correct url and flow

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,10 +2,15 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Partner Portal - Login page', () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto('https://portal-auth-us-east-2.uat.fastgamernetwork.com/login?client_id=k60144cohv4qmhk4qtskm7pse&redirect_uri=https%3A%2F%2Fpartners.uat.fastgamernetwork.com%2Fcallback&response_type=code&scope=openid%20email%20profile%20aws.cognito.signin.user.admin&nonce=5e4bed19f84ea5fbfee103a33b51abf229MGlh0nD&state=d683c23ef8ec42d7238c2cdc09c660aea1wEx62df&code_challenge=R8xW0WYhxvTEtaJkiX9gvJR7mRdeJyW0QNp4pTPJx5s&code_challenge_method=S256', { waitUntil: 'networkidle' });
+    await page.goto('https://partners.uat.fastgamernetwork.com/public/documents', { waitUntil: 'domcontentloaded' });
+    await Promise.all([
+      page.waitForNavigation({ waitUntil: 'domcontentloaded' }),
+      page.getByRole('button', { name: /log in/i }).click(),
+    ]);
   });
 
   test('should display the login form', async ({ page }) => {
+  
     await expect(page.locator('form')).toBeVisible();
     await expect(page.getByLabel('Email Address')).toBeVisible();
     await expect(page.locator('input[name="password"]')).toBeVisible();
